199db7ce5a01ea6e0d01345261c781790e395dd0
Sujil Kumar K.M <kmsujilkumar@gmail.com>
2022-05-25 23:32:12 +0100

    file name changed

---
+++ setup.sh
@@ -0,0 +1,15 @@
+
+
+
+
+# method 1 (going to the folder in my local computer and open powershell and run below command)
+scp dataset.csv sujilkumarkm@data.peadargrant.ie:/home/sujilkumarkm/assignment2/
+
+OR
+
+# method 2 (using git repo given below clone the folder which contains the dataset)
+
+git clone https://github.com/sujilkumarkm/da_assignment2_semester2_2021_dkit.git
+
+
+# please note that both methods are applicable.
